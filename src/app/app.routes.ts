9de49d2eb54e3c049b29305b3dd3285e9e44703e
mahesh-del <maheshkusuma1999@gmail.com>
2025-06-02 20:51:06 +0530

    Added AuthGuard to announcements route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -28,5 +28,6 @@ export const routes: Routes = [
     path: 'announcements',
     loadComponent: () =>
       import('./features/announcements/announcements.component'),
+    canActivate: [AuthGuard],
   },
 ];
